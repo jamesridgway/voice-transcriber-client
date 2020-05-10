dbcf2effcb60df71b7604b89bd40a5ec2c677a80
James Ridgway <james@jamesridgway.co.uk>
2020-05-10 11:34:28 +0100

    pylint fixes

--- voice_transcriber/voice_transcriber.py
+++ voice_transcriber/voice_transcriber.py
@@ -9,14 +9,14 @@ class VoiceTranscriber:
         self.api_key = api_key
 
     def transcribe(self, name, filename):
-        file_ext = os.path.splitext(filename)[1];
+        file_ext = os.path.splitext(filename)[1]
         data = {"name": name + file_ext}
         request = requests.post('https://{}/recordings'.format(self.domain), headers=self.__headers(), json=data)
 
         if request.status_code == 200:
             response = request.json()
             upload_url = response['upload_url']
-            request = requests.put(upload_url, headers=self.__headers(), data=open(filename,'rb').read())
+            request = requests.put(upload_url, headers=self.__headers(), data=open(filename, 'rb').read())
             return request.status_code == 200
 
         raise RuntimeError('An unexpected error occurred')
